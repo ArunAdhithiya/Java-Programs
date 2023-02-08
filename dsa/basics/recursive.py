a512fc3895e85e63cf30e4b8c8cfd2f01e1b0520
Adhi <102973801+ArunAdhithiya@users.noreply.github.com>
2023-02-08 16:50:09 +0530

    First Commit

--- dsa/basics/recursive.py
+++ dsa/basics/recursive.py
@@ -5,3 +5,15 @@ def gcd(m,n):
     return b
   else:
     gcd(a,a-b)
+    
+    
+#Euclids algorithm with number of digits during modulo division determines complexity (better than above approach)
+
+def gcd(m,n):
+  (a,b)=(max(m,n),min(m,n))
+  if a%b==0:
+    return b
+  else:
+    gcd(a,a%b)
+    
+   
