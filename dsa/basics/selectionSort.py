8dcd74f67571d307e712176dfa730caddd549097
Adhi <102973801+ArunAdhithiya@users.noreply.github.com>
2023-02-10 18:13:28 +0530

    First Commit

---
+++ dsa/basics/selectionSort.py
@@ -0,0 +1,12 @@
+def SelectionSort(l):
+   for i in range(len(l)):
+       mi=i
+       for j in range(i+1,len(l)):
+           if(l[mi]>l[j]):
+               mi=j
+
+       (l[i],l[mi])=(l[mi],l[i])
+
+   return l
+
+print(SelectionSort([23,4,3,1,2,35,434,332]))
